4d43c779eacec98f843731dae509ff0ae5ee831b
Administrator <Administrator@WIN-T3IURFIC3HU.localdomain>
2021-01-11 16:34:45 +0800

    update0108

--- bis/src/com/bot/bis/dao/AbstractDao.java
+++ bis/src/com/bot/bis/dao/AbstractDao.java
@@ -19,6 +19,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bot.bis.model.ActionApproves;
